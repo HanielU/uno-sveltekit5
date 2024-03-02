0448abb0378ff88f86889dd8c4a87e59967cfd40
Haniel Ubogu <hanieltobi@gmail.com>
2024-03-02 22:44:12 +0000

    misc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,17 +6,11 @@
 <svelte:window bind:scrollY />
 
 <nav class="fixed {showHideNav} duration-1000 ease w-full">
-	<ul>
+	<ul class="bg-#ff3e00 p-3 flex justify-end gap-2">
 		<li><a target="_self">home</a></li>
 		<li><a target="_self">one</a></li>
 		<li><a target="_self">two</a></li>
 	</ul>
 </nav>
 
-<div class="py-30">
-	<h1>
-		scrollY: {scrollY}
-	</h1>
-</div>
-
-<div class="py-300"></div>
+<div class="h-5000px"></div>
